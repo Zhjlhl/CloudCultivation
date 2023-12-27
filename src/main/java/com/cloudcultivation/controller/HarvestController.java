f19c12600123475b3f161b28f2eef4c9a44a9d28
Jiejie <1270107783@qq.com>
2023-12-27 19:22:23 +0800

    add 一个页面跳转

--- src/main/java/com/cloudcultivation/controller/HarvestController.java
+++ src/main/java/com/cloudcultivation/controller/HarvestController.java
@@ -4,6 +4,7 @@ package com.cloudcultivation.controller;
 import com.cloudcultivation.service.HarvestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * @author Jiejie
@@ -14,5 +15,13 @@ public class HarvestController {
     @Autowired
     private HarvestService harvestService;
 
+
+    /*
+     * @description: 页面跳转
+     */
+    @RequestMapping("/toHarvestInformation")
+    public String toHarvestInformation(){
+        return "customer/harvestOngoingOrder.jsp";
+    }
 
 }
